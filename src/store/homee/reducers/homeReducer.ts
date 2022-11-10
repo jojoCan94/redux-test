da9e53d6a32f3d8c57a3aa530fae09ad2f82c239
Jonathan <jonathan.cannizzaro@sysconsgroup.com>
2022-11-10 17:13:16 +0100

    test merge

--- src/store/homee/reducers/homeReducer.ts
+++ src/store/homee/reducers/homeReducer.ts
@@ -9,12 +9,15 @@ export default function homeReducer(state = [], action: any) {
         case types.GET_HOME_DATA_REQUEST: {
             return {...state}
         }
+
         case types.GET_HOME_DATA_SUCCESS: {
             return {...state, homeDataResponse: action.homeResponse}
         }
+
         case types.GET_HOME_DATA_ERROR: {
             return {...state, errorMsg: "some errors"}
         }
+        
         default: {
             return { ...state }
         }
